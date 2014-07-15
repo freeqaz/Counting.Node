d09a273ef6a4c15dff886845288525cbafb25184
Johnathan Wortley <00free@gmail.com>
2014-07-15 10:30:54 -0700

    Update Worker.js

--- Worker.js
+++ Worker.js
@@ -1,22 +1,20 @@
-// Task worker in node.js
+// Task worker in Node.js
 // Connects PULL socket to tcp://localhost:5557
 // Collects workloads from ventilator via that socket
 // Connects PUSH socket to tcp://localhost:5558
 // Sends results to sink via that socket
 
-var zmq = require('zmq')
-  , receiver = zmq.socket('pull')
-  , sender = zmq.socket('push');
+var zmq = require('zmq'),
+    receiver = zmq.socket('pull'),
+    sender = zmq.socket('push');
 
 receiver.on('message', function (buf) {
     var msec = parseInt(buf.toString(), 10);
 
-    // simple progress indicator for the viewer
+    // Simple progress indicator for the viewer
     process.stdout.write(buf.toString() + ".");
 
-    // do the work
-    // not a great node sample for zeromq,
-    // node receives messages while timers run.
+    // Do the work
     setTimeout(function () {
         sender.send("");
     }, msec);
