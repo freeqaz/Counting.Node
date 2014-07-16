884f481d921b73c72d20872b72c2bdbd4a722264
Free Wortley <00free@gmail.com>
2014-07-16 08:53:00 -0700

    * Started fleshing out Worker.

--- Worker.js
+++ Worker.js
@@ -9,15 +9,32 @@ var zmq = require('zmq'),
     sender = zmq.socket('push');
 
 receiver.on('message', function (buf) {
-    var msec = parseInt(buf.toString(), 10);
+    var taskString = buf.toString();
 
-    // Simple progress indicator for the viewer
-    process.stdout.write(buf.toString() + ".");
+    var tasks = taskString.split(",");
+
+    //process.stdout.write(buf.toString() + ".");
+
+    var job = tasks[0];
+
+    console.log("job" + job);
+
+    var result = {};
+    result.Data = {};
+    for (var i = 1; i < tasks.length; i++) {
+        if (result.Data[tasks[i]] !== undefined) {
+            result.Data[tasks[i]] += 1;
+        } else {
+            result.Data[tasks[i]] = 1;
+        }
+    }
+
+    console.log(result.Data);
 
     // Do the work
-    setTimeout(function () {
-        sender.send("");
-    }, msec);
+    //setTimeout(function () {
+    //    sender.send("");
+    //}, msec);
 });
 
 receiver.connect('tcp://localhost:5557');
